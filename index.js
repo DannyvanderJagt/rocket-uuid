ddcab2670f19948e594ee193ba6bd88550607f4b
Danny van der Jagt <dannyvanderjagt@gmail.com>
2015-10-10 12:55:44 +0200

    [Cleanup] Removed index.js at base folder.

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-    return (
-        "0000" + (
-        (Math.random()*1400 * Math.pow(36,4))+
-        ((new Date().getTime())/10000)
-        )|0)
-    .toString(36).slice(-6);
-}
